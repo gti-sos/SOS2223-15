293654350f373b1470bba002b71fd9e3f103fe12
marmedsan6 <mariomedinasantos1@gmail.com>
2023-03-13 11:07:54 +0100

    F05

--- index.js
+++ index.js
@@ -82,8 +82,86 @@ app.get("/samples/MMS", (req, res)=> {
     console.log("New request");
 });
 
+//Codigo para el F05
+
+//APARTADO CREAR 10 O MÁS DATOS RANDOM
+
+const población_media = [
+        [2022, "Ambos sexos", "Almería", 110379, 60831, 22827],
+        [2022, "Ambos sexos", "Cadíz", 246181, 124697, 26756],
+        [2022, "Ambos sexos", "Sevilla", 403262, 172309, 27654],
+        [2022, "Hombres", "Almería", 51771, 27381, 11507],
+        [2022, "Mujeres", "Córdoba", 90524, 40810, 8721],
+        [2022, "Mujeres", "Almería", 58608, 33450, 11320],
+        [2022, "Mujeres", "Córdoba", 90524, 40810, 8721],
+        [2022, "Mujeres", "Sevilla", 200597, 101940, 13677],
+        [2022, "Mujeres", "Jaén", 79768, 33345, 10257],
+        [2022, "Mujeres", "Huelva", 80325, 30317, 9049],
+        [2022, "Hombres", "Cadíz", 124802, 51697, 14619],
+        [2022, "Hombres", "Córdoba", 88957, 27166, 11413],
+        [2022, "Hombres", "Granada", 83366, 35666, 9755],
+        [2022, "Hombres", "Huelva", 68019, 21665, 7518],
+        [2022, "Hombres", "Sevilla", 195063, 70369, 13977]];
+
+//TABLA AZUL
+const rutaMMS = '/loss-jobs';
+
+//GET a ruta loadInitialData (crea datos si no los hay ya creados).
+var crea_datos = [];
+app.get(recurso_url + "/loadInitialData", (request, response) => {
+if(crea_datos.length === 0){
+    crea_datos.push(población_media);
+    response.json(crea_datos);
+    console.log("Se han creado datos para /loss-jobs");
+} else {
+    response.send('Esta ruta ya contiene datos');
+    console.log('Esta ruta ya contiene datos');
+}
+});
+
+// GET al recurso loss-jobs
+
+app.get(rutaMMS, (request, response) => {
+  response.json(población_media);
+  console.log("New GET to /loss-jobs");
+  response.sendStatus(200);
+});
+
+//POST al recurso
+app.post(rutaMMS, (request, response) => {
+  var newEntry_mms = request.body;
+  console.log(`newEntry = ${JSON.stringify(newEntry_amr,null,2)}`);
+  console.log("New POST to /loss-jobs");
+  población_media.push(newEntry_mms);
+  response.sendStatus(201).send('Nuevo dato creado correctamente');
+});
 
 
+//PUT al recurso
+app.put(rutaMMS, (request, response) => {
+  response.sendStatus(405).send('No se permite hacer un PUT en esta ruta');
+});
+
+//DELETE al recurso
+app.delete(rutaMMS, (request, response) => {
+  población_media = [];
+  console.log("New DELETE to /loss-jobs");
+  response.sendStatus(200);
+});
+
+
+//No se puede hacer POST a loadInitialData
+app.post(rutaMMS + "/loadInitialData", (request, response) => {
+  response.sendStatus(405).send('No se permite hacer un POST en esta ruta');
+});
+
+
+//GET a todas las estadísticas de Cordoba
+app.get(rutaMMS + "/Cordoba", (request, response) => {
+  console.log(response.json(población_media.filter(dato => dato[0] === 'Córdoba')));
+  console.log("New GET to /loss-jobs/Cordoba");
+  response.sendStatus(200);
+});
 
 //ruta AMR
 
