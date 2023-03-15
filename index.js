c2ed8f20cce6114c1e9529a2a4c2342b40ca9838
angmarrod4 <angmarrod4@alum.us.es>
2023-03-15 20:26:53 +0100

    Algunos endpoints añadidos

--- index.js
+++ index.js
@@ -214,33 +214,36 @@ app.get(rutaMMS + "/Cordoba", (request, response) => {
 //-------------------------------------------------Parte Angel--------------------------------------------------------
 
 
-const salario_medio = [{"province":"Almería", "gender":"male", "year":2021, "salaried":162.525, "average_salary":21.311, "standard_deviation":12.172},
-{"province":"Almería", "gender":"female", "year":2021, "salaried":133.150, "average_salary":20.786, "standard_deviation":10.696},
-{"province":"Cádiz","gender":"male","year": 2021, "salaried":237.225, "average_salary":25.200, "standard_deviation":14.633},
-{"province":"Cádiz", "gender":"female", "year":2021, "salaried":197.100, "average_salary":22.189, "standard_deviation":10.835},
-{"province":"Córdoba", "gender":"male", "year":2021, "salaried":159.800,"average_salary":23.220, "standard_deviation":12.819},
-{"province":"Córdoba", "gender":"female", "year":2021, "salaried":138.800, "average_salary":21.573, "standard_deviation":10.790},
-{"province":"Granada", "gender":"male", "year":2021, "salaried":177.625, "average_salary":24.186, "standard_deviation":13.778}, 
-{"province":"Granada", "gender":"female", "year":2021, "salaried":161.125, "average_salary":22.691, "standard_deviation":11.540},
-{"province":"Huelva", "gender":"male", "year":2021, "salaried":124.500, "average_salary":22.875, "standard_deviation":12.677},
-{"province":"Huelva", "gender":"female", "year":2021, "salaried":126.975, "average_salary":19.305, "standard_deviation":8.513},
-{"province":"Almería", "gender":"male", "year":2020, "salaried":156.725, "average_salary":21.163, "standard_deviation":11.718},
-{"province":"Almería", "gender":"female", "year":2020, "salaried":128.225, "average_salary":20.535,"standard_deviation":10.149}];
+var salario_medio = [
+{province:"Almería", gender:"male", year:2021, salaried:162.525, average_salary:21.311, standard_deviation:12.172},
+{province:"Almería", gender:"female", year:2021, salaried:133.150, average_salary:20.786, standard_deviation:10.696},
+{province:"Cádiz",gender:"male",year: 2021, salaried:237.225, average_salary:25.200, standard_deviation:14.633},
+{province:"Cádiz", gender:"female", year:2021, salaried:197.100, average_salary:22.189, standard_deviation:10.835},
+{province:"Córdoba", gender:"male", year:2021, salaried:159.800,average_salary:23.220, standard_deviation:12.819},
+{province:"Córdoba", gender:"female", year:2021, salaried:138.800, average_salary:21.573, standard_deviation:10.790},
+{province:"Granada", gender:"male", year:2021, salaried:177.625, average_salary:24.186, standard_deviation:13.778}, 
+{province:"Granada", gender:"female", year:2021, salaried:161.125, average_salary:22.691, standard_deviation:11.540},
+{province:"Huelva", gender:"male", year:2021, salaried:124.500, average_salary:22.875, standard_deviation:12.677},
+{province:"Huelva", gender:"female", year:2021, salaried:126.975, average_salary:19.305, standard_deviation:8.513},
+{province:"Almería", gender:"male", year:2020, salaried:156.725, average_salary:21.163, standard_deviation:11.718},
+{province:"Almería", gender:"female", year:2020, salaried:128.225, average_salary:20.535, standard_deviation:10.149}];
 
 
 const recurso_amr = BASE_API_URL+"/andalusian-population-salary-stats";
 
+// GETs ------------------------------
+
 
 //GET a ruta loadInitialData (crea datos si no los hay ya creados).
 var crea_datos = [];
-app.get(recurso_amr + "/loadInitialData", (request, response) => {
+app.get(recurso_amr + "/loadInitialData/", (request, response) => {
 if(crea_datos.length === 0){
-    crea_datos.push(salario_medio);
-    response.json(crea_datos);
+    response.json(salario_medio.slice(",")); //No mostramos crea_datos.push(salario_medio.slice()) porque lo estaría metiendo todo dentro de una lista.
+    crea_datos.push(salario_medio.slice(","));
     console.log("Se han creado datos para /andalusian-population-salary-stats/loadInitialData");
 } else {
-    response.send('Esta ruta ya contiene datos');
-    console.log('Esta ruta ya contiene datos');
+    response.send(`Esta ruta ya contiene datos: ${JSON.stringify(salario_medio.slice(","))}`);
+    console.log('Esta ruta ya contiene datos'); //Mensaje por consola.
 }
 });
 
@@ -254,7 +257,43 @@ app.get(recurso_amr, (request, response) => {
     response.sendStatus(200);
 });
 
+//GET a un recurso concreto usando filtro de año.
 
+app.get(recurso_amr+"/:year", (request, response) => {
+    const year = request.params.year.slice([5]); // Con esto, obtenemos el valor de la clave "year" de la url, si es que se le pasa dicho parámetro al realizar la petición (en realidad devuelve "year=año", por eso se hace el slicing)
+    if(year != undefined){
+        response.json(salario_medio.filter( m => m.year == year));
+        console.log(`New Get to /andalusian-population-salary-stats filtering by year (${year})`);
+        //response.sendStatus(200);
+    }
+    else{
+        response.json(salario_medio);
+        console.log("New GET to /andalusian-population-salary-stats", year);
+        //response.sendStatus(200);
+    }
+});
+
+
+// GET con rango de fecha
+
+app.get(recurso_amr + '/andalusian-population-salary-stats', (req, res) => {
+    const { from, to } = req.query;
+    let filteredCampings = salario_medio;
+    if (from && to) {
+      filteredCampings = salario_medio.filter(camping => {
+        const year = salario_medio.year;
+        return year >= from && year <= to;
+      });
+    }
+    if (filteredCampings.length === 0) {
+      return res.status(404).json({ error: 'No campings found.' });
+    }
+    res.json(filteredCampings);
+  });
+
+
+
+//POSTs --------------------------------------------
 
 //POST al recurso
 app.post(recurso_amr, (request, response) => {
@@ -265,6 +304,8 @@ app.post(recurso_amr, (request, response) => {
     response.sendStatus(201).send('Nuevo dato creado correctamente');
 });
 
+
+//PUTs ----------------------------------------------
 
 //PUT al recurso
 app.put(recurso_amr, (request, response) => {
