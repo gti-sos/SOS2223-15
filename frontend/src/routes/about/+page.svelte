29a8df9a18fd7dcd3ac38a35e12eaec81ffabc2d
marmedsan6 <mariomedinasantos1@gmail.com>
2023-03-30 13:32:46 +0200

    L09FRONTEND

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+
+    <h1>Prueba</h1>
+    Made with love from <a href= "https://es.wikipedia.org/wiki/Sevilla"> enlace </a>
+    by <a href = "https://github.com/gti-sos"> SOS Team</a>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+
+</style>
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var cool = require("cool-ascii-faces");
-var bodyParser = require("body-parser");
+import express from "express";
+//var cool = require("cool-ascii-faces");
+//var bodyParser = require("body-parser"); //En express han añadido bodyparser
 var mudulo_jara = require("./backend/api_jara");
-var modulo_mario = require("./backend/api_mario");
+//var modulo_mario = require("./backend/api_mario");
+import { loadBackendMMS } from "./backend/api_mario.js"; //FRONTEND
 var modulo_angel = require("./backend/api_angel");
-const Datastore = require('nedb');
+import Datastore from 'nedb';
+import { handler } from "./frontend/build/handler.js";
 
 
 var app = express();
@@ -12,12 +14,14 @@ var port = process.env.PORT || 12345;
 
 // Modularización--------------------
 
-app.use(bodyParser.json()); //Importante colocar esto antes de los modulo_fff(app) para que funcione bien.
+app.use(express.json()); //Importante colocar esto antes de los modulo_fff(app) para que funcione bien.
 app.use("/", express.static("./public")); // HTML que se mostrará por defecto en la ruta /
 
 mudulo_jara(app);
-modulo_mario(app);
+loadBackendMMS(app);
 modulo_angel(app);
+
+app.use(handler);
 
 // ----------------------------------
 
