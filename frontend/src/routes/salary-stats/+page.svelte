bcbeb77ecc900f32356788cd101a15a8c11a8786
angmarrod4 <angmarrod4@alum.us.es>
2023-04-07 14:08:50 +0200

    Delete implementado y nuevos mensajes para el usuario

--- frontend/src/routes/salary-stats/+page.svelte
+++ frontend/src/routes/salary-stats/+page.svelte
@@ -8,7 +8,7 @@
 
     onMount(async () => {
         // Esto carga el getSalary_stats nada mas iniciar la aplicación.
-        getSalary_stats();
+        getSalaryStats();
     });
 
     let API = "/api/v1/salary-stats";
@@ -29,7 +29,7 @@
     let resultStatus = "";
     let message = "";
 
-    async function getSalary_stats() {
+    async function getSalaryStats() {
         resultStatus = result = "";
         const res = await fetch(API, { // fetch (url)
             method: "GET"
@@ -43,9 +43,14 @@
         }
         const status = await res.status;
         resultStatus = status;
+        if(status == 200){
+            message = `Obtenidos todos los recursos correctamente.`
+        } else if(status==404){
+            message = `No hay datos en la base de datos.`
+        }
     }
 
-    async function createContact() {
+    async function createSalary() {
         resultStatus = result = "";
         const res = await fetch(API, {
             method: "POST",
@@ -64,14 +69,36 @@
         const status = await res.status;
         resultStatus = status;
         if (status == 201) {
-            getSalary_stats();
+            getSalaryStats();
         }else if(status==400){
-            message = "Faltan campos por rellenar.";
-            console.log("ERROR. Missing one or more fields.")
+            message = `Faltan campos por rellenar: provincia: ${res.body.province}, género: ${res.body.gender}, año: ${res.body.year},
+             número de asalariados: ${res.body.salaried}, salario medio: ${res.body.average_salary}, desviación típica: ${res.body.standard_deviation} `;
+            console.log("ERROR. Missing one or more fields.");
         }
         else if(status==409){
-            message = "Ya existe ese recurso";
-            console.log("ERROR. There is already a resoruce with the given id (province, gender and year) in the data base.")
+            message = `Ya existe el recurso que se quiere introducir: ${res.body.province} ${res.body.gender} ${res.body.year}`;
+            console.log("ERROR. There is already a resoruce with the given id (province, gender and year) in the data base.");
+        }
+    }
+
+    async function deleteAllStats() {
+        resultStatus = result = "";
+        const res = await fetch(API, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const status = await res.status;
+        resultStatus = status;
+        if(status==200){
+            getSalaryStats();
+            message = `Se han borrado correctamente los datos de la base de datos.`;
+            console.log("Deleted all resources correctly.");
+        }
+        else if(status==500){
+            message = `Error en el cliente.`;
+            console.log("ERROR. Client error.");
         }
     }
 </script>
@@ -97,7 +124,8 @@
             <td><input bind:value={newSalaried} /></td>
             <td><input bind:value={newAverage_salary} /></td>
             <td><input bind:value={newStandard_deviation} /></td>
-            <td><Button on:click={createContact}>Crear</Button></td>
+            <td><Button on:click={createSalary}>Crear</Button></td>
+            <td><Button on:click={deleteAllStats}>Borrar</Button></td>
         </tr>
 
         {#each salary_stats as salary_stat}
@@ -114,10 +142,26 @@
     </tbody>
 </Table>
 
-{#if message != ""}
+{#if message != "" && (resultStatus == 200 || resultStatus == 201)} <!--Alerta para los códigos 200 y 201-->
+
+    <div class= "container text-center">
+        <Alert color="success" dismissible>{message}</Alert>
+    </div>
+
+{/if}
+
+{#if message != "" && ((resultStatus != 200 || resultStatus != 201 || resultStatus != 500))} <!--Alerta para los códigos 400,404, ...-->
 
     <div class= "container text-center">
         <Alert color="warning" dismissible>{message}</Alert>
+    </div>
+
+{/if}
+
+{#if message != "" && resultStatus == 500} <!--Alerta para los códigos 500 (internal server error)-->
+
+    <div class= "container text-center">
+        <Alert color="danger" dismissible>{message}</Alert>
     </div>
 
 {/if}
