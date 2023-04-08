84bb9360945fafe9bdb014853a9c4aa23cd4eeca
angmarrod4 <angmarrod4@alum.us.es>
2023-04-08 18:31:36 +0200

    Cambios

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+    <h2>Equipo</h2>
+    <ul>
+        <li><a href="https://github.com/AngelUs04">Ángel Marín Rodríguez</a> : salary-stats</li>
+        <li><a href="https://github.com/jardelpac01">Jara Xiao del Viejo Pacheco</a> : jobseekers-studies</li>
+        <li><a href="https://github.com/marmedsan6">Mario Medina Santos</a> : loss-jobs</li>
+    </ul>
+
+    <h2>Front-ends</h2>
+    <ul>
+        <li><a href="/salary-stats">/salary-stats</a></li>
+	    <li><a href="/jobseekers-studies">/jobseekers-studies</a></li>
+        <li><a href="/loss-jobs">/loss-jobs</a></li>
+    </ul>
+    <h2>APIs</h2>
+    <ul>
+        <li><a href="https://sos2223-15.ew.r.appspot.com/api/v1/salary-stats">/salary-stats</a></li>
+	    <li><a href="https://sos2223-15.ew.r.appspot.com/api/v1/jobseekers-studies">/jobseekers-studies</a></li>
+        <li><a href="https://sos2223-15.ew.r.appspot.com/api/v1/loss-jobs">/loss-jobs</a></li>
+    </ul>
+
+    <h2>Repositorio</h2>
+    <ul>
+        <li><a href="https://github.com/gti-sos/SOS2223-15">gti-sos/SOS2223-15</a></li>
+    </ul>
+    <h2>Documentación POSTMAN</h2>
+    <ul>
+        <li>
+            <a href="https://sos2223-15.appspot.com/api/v1/salary-stats/docs">/salary-stats/docs</a> Desarrollado por : Ángel Marín Rodríguez
+        </li>
+        <li>
+            <a href="https://sos2223-15.ew.r.appspot.com/api/v1/jobseekers-studies/docs">/jobseekers-studies/docs</a> Desarrollado por: Jara Xiao del Viejo Pacheco
+        </li>
+        <li>
+            <a href="https://sos2223-15.ew.r.appspot.com/api/v1/loss-jobs/docs">/loss-jobs/docs</a> Desarrollado por: Mario Medina Santos
+        </li>
+    </ul>
+</section>
+
+<style>
+    h2 {
+        font-size: 24px;
+        font-weight: normal;
+        margin-top: 15px;
+        margin-left: 2%;
+        color: #15297c;
+        list-style-type: circle;
+    }
+    ul {
+        margin: 0;
+        margin-left: 2%;
+        padding: 0;
+        list-style: none;
+    }
+    li {
+        margin: 10px 0;
+        list-style-type: circle;
+    }
+    a {
+        color: #1e90ff;
+        text-decoration: none;
+        font-weight: bold;
+        list-style-type: circle;
+    }
+    section {
+        padding: 20px;
+        max-width: 800px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 0px 5px 8px 5px rgba(0, 0, 0, 0.1);
+        margin-bottom: 1%;
+        margin-top: 1.5%;
+    }
+</style>
