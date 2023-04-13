6d77510d683eaa890bc85eb0fefd75bacf50902a
Jara del Viejo <jardelpac@alum.us.es>
2023-04-13 15:52:43 +0200

    cambios

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -3,21 +3,25 @@
 
 </script>
 
-<Container fluid>
+
     <nav>
-        <Row>
+        <Row style="text-align: center">
             <Col xs="auto"><a href="/">Home</a></Col>
             <Col xs="auto"><a href="/salary-stats">salary-stats</a></Col>
-            <Col xs="auto"><a href="/jobseekers-studies">jobseekers-studies</a></Col>
+            <Col xs="auto"><a href="/jobseekers-studies">Demandantes de empleo</a></Col>
             <Col xs="auto"><a href="/loss-jobs">loss-jobs</a></Col>
         </Row>
     </nav>
-</Container>
 
 <style>
     a{
-        color : red;
+        color: #1e90ff;
+        text-decoration: none;
     }
-    
+    nav{
+        text-align: center;
+        font-size: 30px;
+        font-family: 'Times New Roman', Times, serif;
+    }
 </style>
 
--- frontend/src/routes/jobseekers-studies/+page.svelte
+++ frontend/src/routes/jobseekers-studies/+page.svelte
@@ -19,14 +19,14 @@
         API = "http://localhost:12345" + API;
 
     let jobseekers = [];
-    let newYear = 'year';
-    let newGender = 'gender';
-    let newTerritory = 'territto';
-    let newType = 'type';
-    let newPrimary = 'primary';
-    let newFp_program = 'fp_program';
-    let newGeneral_education = 'general_education';
-    let newTotal = 'total';
+    let newYear = "";
+    let newGender = "";
+    let newTerritory = "";
+    let newType = "";
+    let newPrimary = "";
+    let newFp_program = "";
+    let newGeneral_education = "";
+    let newTotal = "";
 
     let result = "";
     let resultStatus = "";
@@ -123,9 +123,9 @@
     }
 </script>
 
-<h1>jobseekers-studies</h1>
+<h1>Demandantes de empleo</h1>
 
-<Table>
+<Table hover responsive size="sm">
     <thead>
         <tr>
             <th>Año</th>
@@ -140,39 +140,39 @@
     </thead>
     <tbody>
         <tr>
-            <td><input bind:value={newYear} /></td>
-            <td><input bind:value={newGender} /></td>
-            <td><input bind:value={newTerritory} /></td>
-            <td><input bind:value={newType} /></td>
-            <td><input bind:value={newPrimary} /></td>
-            <td><input bind:value={newFp_program} /></td>
-            <td><input bind:value={newGeneral_education} /></td>
-            <td><input bind:value={newTotal} /></td>
-            <td><Button on:click={createJobseeker}>Crear</Button></td>
-            <td><Button on:click={deleteResource(newYear,newGender,newTerritory,newType)}>Borrar recurso</Button></td>
-            <td><Button on:click={deleteAllStats}>Borrar</Button></td>
+            <td><input style="width: 50px;" placeholder="Año" bind:value={newYear} /></td>
+            <td><input style="width: 100px;" placeholder="Género" bind:value={newGender} /></td>
+            <td><input style="width: 90px;" placeholder="Territorio" bind:value={newTerritory} /></td>
+            <td><input placeholder="Tipo demandante" bind:value={newType} /></td>
+            <td><input style="width: 150px;" placeholder="Estudios primarios" bind:value={newPrimary} /></td>
+            <td><input style="width: 100px;" placeholder="Programa FP" bind:value={newFp_program} /></td>
+            <td><input style="width: 150px;"placeholder="Educación general" bind:value={newGeneral_education} /></td>
+            <td><input style="width: 50px;" placeholder="Total" bind:value={newTotal} /></td>
+            <td><Button color="primary" on:click={createJobseeker}>Crear</Button></td>
+            <!--<td><Button color="danger" on:click={deleteAllStats}>Borrar datos</Button></td>-->
         </tr>
 
         {#each jobseekers as jobseekers}
             <tr>
-                <td><a href="/jobseekers-studies/{jobseekers.year}/{jobseekers.gender}/{jobseekers.territory}/{jobseekers.type}">{jobseekers.territory}</a></td>
                 <td>{jobseekers.year}</td>
                 <td>{jobseekers.gender}</td>
-                <td>{jobseekers.territory}</td>
-                <td>{jobseekers.type}</td>
+                <td><a href="/jobseekers-studies/{jobseekers.year}/{jobseekers.gender}/{jobseekers.territory}/{jobseekers.type}">{jobseekers.territory}</a></td>
+                <td style="width: 90px;">{jobseekers.type}</td>
                 <td>{jobseekers.primary}</td>
                 <td>{jobseekers.fp_program}</td>
                 <td>{jobseekers.general_education}</td>
                 <td>{jobseekers.total}</td>
-                <td><Button on:click={deleteResource(jobseekers.year,jobseekers.gender,jobseekers.territory,jobseekers.type)}>Borrar recurso</Button></td>
-                <td>&nbsp</td>
+                <td><Button color="danger" on:click={deleteResource(jobseekers.year,jobseekers.gender,jobseekers.territory,jobseekers.type)}>Borrar</Button></td>
             </tr>
         {/each}
     </tbody>
 </Table>
 
-{#if message != "" && (resultStatus == 200 || resultStatus == 201)} <!--Alerta para los códigos 200 y 201-->
+<div style="text-align: center;">
+    <button type="button" class="btn btn-danger" on:click={deleteAllStats}>Borrar Datos</button>
+</div>
 
+{#if message != "" && (resultStatus == 200 || resultStatus == 201)} <!--Alerta para los códigos 200 y 201-->
     <div class= "container text-center">
         <Alert color="success" dismissible>{message}</Alert>
     </div>
@@ -194,6 +194,8 @@
     </div>
 
 {/if}
+
+
 
 {#if resultStatus != ""}
     <p>Result:</p>
