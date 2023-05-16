49b10881c7700a89b187cb563d1a3d3991095500
angmarrod4 <angmarrod4@alum.us.es>
2023-05-16 12:44:51 +0200

    Gráfica salary-stats

---
+++ frontend/src/routes/graficas/salary-stats/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    //import C3JS from "./C3JS.svelte";
+</script>
+
+<main>
+    <slot></slot>
+    <hr style="margin-left: 100px; margin-right: 100px">
+    <!-- <ApexChart></ApexChart> -->
+</main>
